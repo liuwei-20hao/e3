cdabfb31ee30751c1a79799ee0671823d1ec8016
liuwei <liuwei20hao@163.com>
2018-07-18 15:13:04 +0800

    20180718 聚合项目

--- e3_manage_web/src/main/java/com/e3/controller/ItemController.java
+++ e3_manage_web/src/main/java/com/e3/controller/ItemController.java
@@ -7,14 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-/**
- * 商品管理Controller
- * <p>Title: ItemController</p>
- * <p>Description: </p>
- * <p>Company: www.itcast.cn</p> 
- * @version 1.0
- */
 @Controller
 public class ItemController {
 
